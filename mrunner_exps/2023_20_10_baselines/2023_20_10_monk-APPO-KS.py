90faa3e8911aef450d76073727f453ccccfd4c8b
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-10-20 16:33:19 +0200

    smoke baseline tests

---
+++ mrunner_exps/2023_20_10_baselines/2023_20_10_monk-APPO-KS.py
@@ -0,0 +1,47 @@
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "env": "challenge",
+    "exp_tags": [name],
+    "exp_point": "monk-APPO-KS",
+    "train_for_env_steps": 2_000_000_000,
+    "group": "monk-APPO-KS",
+    "character": "mon-hum-neu-mal",
+    "num_workers": 32,
+    "num_envs_per_worker": 30,
+    "worker_num_splits": 2,
+    "rollout": 32,
+    "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
+    "async_rl": True,
+    "serial_mode": False,
+    "save_milestones_ith": 10_000_000,
+    "wandb_user": "bartekcupial",
+    "wandb_project": "sf2_nethack",
+    "wandb_group": "gmum",
+    "with_wandb": True,
+    "kickstarting_loss_coeff": 0.05,
+    "teacher_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/@-AA-BC/pretrained",
+    "run_teacher_hs": False,
+}
+
+# params different between exps
+params_grid = [
+    {
+        "seed": list(range(1)),
+    },
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="sf2_nethack",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
