04ecd0bc912414eadebd25bb1e617d217b031d02
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-11-26 21:18:07 +0100

    tidy configs

--- mrunner_exps/2023_11_26_fix/2023_26_11_monk-APPO-CEAA-T.py
+++ mrunner_exps/2023_11_26_fix/2023_26_11_monk-APPO-CEAA-T.py
@@ -6,11 +6,11 @@ name = globals()["script"][:-3]
 config = {
     "env": "challenge",
     "exp_tags": [name],
-    "exp_point": "monk-APPO-KLAA-T",
+    "exp_point": "monk-APPO-CEAA-T",
     "train_for_env_steps": 2_000_000_000,
-    "group": "monk-APPO-KLAA-T",
+    "group": "monk-APPO-CEAA-T",
     "character": "mon-hum-neu-mal",
-    "num_workers": 16,
+    "num_workers": 32,
     "num_envs_per_worker": 30,
     "worker_num_splits": 2,
     "rollout": 32,
@@ -22,36 +22,22 @@ config = {
     "wandb_project": "sf2_nethack",
     "wandb_group": "gmum",
     "with_wandb": True,
-    # "use_dataset": True,
+    "use_dataset": True,
     "db_path": "/ttyrecs/ttyrecs.db",
     "data_path": "/nle/nld-aa/nle_data",
     "dataset_name": "autoascend",
-    # "dataset_rollout": 32,
-    # "dataset_batch_size": 8192,  # this equals bs = 256, 256 * 32 = 8192
-    # "distillation_loss_coeff": 0.2,
-    # "teacher_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/@-AA-BC/pretrained_use_prev_action",
-    "run_teacher_hs": False,
-    "use_prev_action": True,
-    "model": "ScaledNet",
-    "use_resnet": True,
-    "rnn_size": 1738,
+    "dataset_rollout": 32,
+    "dataset_batch_size": 4096,  # this equals bs = 256, 256 * 32 = 8192
+    "dataset_num_splits": 2,
+    "supervised_loss_coeff": 0.5,
 }
 
 # params different between exps
 params_grid = [
     {
-        "seed": list(range(1)),
-        "num_workers": [8],
-        "batch_size": [512],
-        "dataset_batch_size": [1024],
+        "seed": list(range(2)),
         "use_pretrained_checkpoint": [True],
-        "teacher_path": ["/home/bartek/Workspace/data/sf_checkpoints/amzn-AA-BC/pretrained_use_prev_action"],
-        "model_path": ["/home/bartek/Workspace/data/sf_checkpoints/amzn-AA-BC/pretrained_use_prev_action"],
-        "db_path": ["/home/bartek/Workspace/data/nethack/AA-taster/ttyrecs.db"],
-        # "serial_mode": [True],
-        "with_wandb": [False],
-        "restart_behavior": ["overwrite"],
-        "learning_rate": [0.0],
+        "model_path": ["/raid/NFS_SHARE/results/bartlomiej.cupial/sf_checkpoints/@-AA-BC/pretrained"],
     },
 ]
 
