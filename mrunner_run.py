310a21c47807de744c7b958558a4623f7a0837a1
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-12-14 11:49:16 +0100

    add option for other scripts to use mrunner

--- mrunner_run.py
+++ mrunner_run.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     cfg = get_configuration(print_diagnostics=True, with_neptune=False)
 
     del cfg["experiment_id"]
+    run_script = cfg.pop("run_script", "sf_examples.nethack.train_nethack")
 
     key_pairs = [f"--{key}={value}" for key, value in cfg.items()]
-    cmd = ["python", "-m", "sf_examples.nethack.train_nethack"] + key_pairs
+    cmd = ["python", "-m", run_script] + key_pairs
     subprocess.run(cmd)
