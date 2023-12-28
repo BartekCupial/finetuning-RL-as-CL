53a47be1dfae22939e315a1658636326f6611f65
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-12-28 11:49:32 +0100

    todo runs

--- mrunner_auto/2023_12_28_baselines.sh
+++ mrunner_auto/2023_12_28_baselines.sh
@@ -2,7 +2,13 @@
 
 ssh-add
 
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-CEAA.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KLAA.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KS.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO.py
+# TODO:
+mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-CEAA-T.py
+mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KLAA-T.py
+mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KS-T.py
+mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-T.py
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-CEAA.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KLAA.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO-KS.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2023_12_28_baselines/2023_12_28_monk-APPO.py
