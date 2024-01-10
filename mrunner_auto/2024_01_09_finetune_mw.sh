be4c431cb55debd3c283afb2b6f9d104233a14ed
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-10 18:36:04 +0100

    experiments with different warmup and turn off normalization of the returns

--- mrunner_auto/2024_01_09_finetune_mw.sh
+++ mrunner_auto/2024_01_09_finetune_mw.sh
@@ -9,3 +9,8 @@ ssh-add
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-T_batch_norm.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-KS-T_batch_norm.py
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-T_no_warmup.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-KS-T_no_warmup.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-T_normalize_returns.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_09_finetune_mw/2024_01_10_monk-APPO-KS-T_normalize_returns.py
