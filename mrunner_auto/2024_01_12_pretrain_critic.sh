9c7ee69e6e72b75ab6558cd3da0c201c7301731f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-12 13:54:54 +0100

    pretrain critic exp

---
+++ mrunner_auto/2024_01_12_pretrain_critic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh-add
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_pretrain_critic/2024_01_12_monk-APPO-T-pretrain_critic.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_pretrain_critic/2024_01_12_monk-APPO-T-pretrain_critic_no_reward_clip.py
