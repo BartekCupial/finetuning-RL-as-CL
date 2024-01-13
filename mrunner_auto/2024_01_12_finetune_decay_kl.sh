9ad2983846a09db75e14fee11571a999fc7603a4
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-13 18:14:45 +0100

    additional exps with ks decay

--- mrunner_auto/2024_01_12_finetune_decay_kl.sh
+++ mrunner_auto/2024_01_12_finetune_decay_kl.sh
@@ -3,3 +3,4 @@
 ssh-add
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_12_monk-APPO-KS-T-freeze-decay.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_12_monk-APPO-KS-T-freeze-decay-returns.py
