7dca48eb94c7b8fb945192c8a20ca87c2f3e2a42
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-15 11:30:01 +0100

    more experiments with gamma 1.0

--- mrunner_auto/2024_01_12_finetune_decay_kl.sh
+++ mrunner_auto/2024_01_12_finetune_decay_kl.sh
@@ -8,3 +8,6 @@ ssh-add
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_12_monk-APPO-KS-T-freeze-decay-reward_scale.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_12_monk-APPO-KS-T-freeze_separate-decay.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_12_monk-APPO-KS-T-freeze_encoder_only-decay.py
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_15_monk-APPO-KS-T-freeze-decay-gamma-reward.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_12_finetune_decay_kl/2024_01_15_monk-APPO-KS-T-freeze_encoder_only-decay-gamma.py
