3fbf6388db99f7d7e395f7bba1350a4072501f78
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-16 12:42:59 +0100

    exps improving to 10k

---
+++ mrunner_auto/2024_01_15_finetune_improve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ssh-add
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_15_finetune_improve/2024_01_15_monk-APPO-KS-T-init_critic_head.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_15_finetune_improve/2024_01_15_monk-APPO-KS-T-ppo_epochs.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_15_finetune_improve/2024_01_15_monk-APPO-KS-T-reward.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_15_finetune_improve/2024_01_15_monk-APPO-KS-T-separate.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/2024_01_15_finetune_improve/2024_01_15_monk-APPO-KS-T-unfreeze_everything.py
