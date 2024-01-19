6e9bafd49deb9b5382a231a5f58c58b4ab0bc57f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-19 15:10:19 +0100

    finetune on pretrained critic with no reward clip

---
+++ mrunner_exps/2024_01_18_finetune_better/2024_01_18_monk-APPO-KS-T-skip_train-use_pretrained_critic-no_reward_clip.py
@@ -0,0 +1,65 @@
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "env": "challenge",
+    "exp_tags": [name],
+    "exp_point": "monk-APPO-KS-T",
+    "train_for_env_steps": 500_000_000,
+    "group": "monk-APPO-KS-T",
+    "character": "mon-hum-neu-mal",
+    "num_workers": 16,
+    "num_envs_per_worker": 32,
+    "worker_num_splits": 2,
+    "rollout": 32,
+    "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
+    "async_rl": True,
+    "serial_mode": False,
+    "wandb_user": "bartekcupial",
+    "wandb_project": "sf2_nethack",
+    "wandb_group": "gmum",
+    "with_wandb": True,
+    "use_pretrained_checkpoint": True,
+    "model_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/pretrain_critic_01_18_no_reward_clip",
+    "kickstarting_loss_coeff": 0.5,
+    "teacher_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/pretrain_critic_01_18_no_reward_clip",
+    "run_teacher_hs": False,
+    "use_prev_action": True,
+    "model": "ScaledNet",
+    "use_resnet": True,
+    "rnn_size": 1738,
+    "h_dim": 1738,
+    "exploration_loss_coeff": 0.0,
+    "gamma": 1.0,
+    "reward_scale": 0.01,
+    "kickstarting_loss_decay": 0.99998,
+    "min_kickstarting_loss_coeff": 0.25,
+    "skip_train": 25_000_000,
+}
+
+# params different between exps
+params_grid = [
+    {
+        "seed": list(range(1)),
+        "learning_rate": [0.0001, 0.00001],
+    },
+    {
+        "seed": list(range(1)),
+        "learning_rate": [0.0001, 0.00001],
+        "freeze": [{"encoder": 0}],
+    },
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="sf2_nethack",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
