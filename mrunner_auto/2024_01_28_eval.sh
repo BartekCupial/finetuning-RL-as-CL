a315f72f125eb5c468fde62b38b3d457625c7adf
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-01-31 08:23:32 +0100

    more extensive eval on the last checkpoint

--- mrunner_auto/2024_01_28_eval.sh
+++ mrunner_auto/2024_01_28_eval.sh
@@ -8,11 +8,16 @@ ssh-add
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-BC-T.py
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-T.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-KS-T.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-KS-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-BC-T.py
 
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-T.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-KS-T.py
-mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-KS-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-BC-T.py
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_31_eval_APPO.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_31_eval_APPO-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_31_eval_APPO-KS-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_31_eval_APPO-BC-T.py
---
+++ mrunner_exps/eval/2024_01_31_eval_APPO-BC-T.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+from sf_examples.nethack.utils.paramiko import get_checkpoint_paths
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "run_script": "sf_examples.nethack.eval_nethack",
+    "env": "challenge",
+    "exp_tags": [name],
+    "character": "mon-hum-neu-mal",
+    "with_wandb": True,
+    "use_pretrained_checkpoint": False,
+    "load_checkpoint_kind": "latest",
+    "train_dir": "/home/bartek/Workspace/ideas/sample-factory/train_dir",
+    "experiment": "amzn-AA-BC_pretrained",
+    "sample_env_episodes": 200,
+    "num_workers": 16,
+    "num_envs_per_worker": 1,
+    "worker_num_splits": 1,
+    "restart_behavior": "overwrite",
+}
+
+csv_folder_name = f"{config['character']}_episodes{config['sample_env_episodes']}"
+
+checkpoints = get_checkpoint_paths("/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_exp/appo-bc-t-baseline/")
+checkpoints = list(map(Path, checkpoints))
+checkpoints = list(filter(lambda p: p.parent.name in ["default_experiment"], checkpoints))
+
+# params different between exps
+params_grid = [
+    {
+        "csv_folder_name": [csv_folder_name],
+        "train_dir": [str(checkpoint.parent.parent)],
+        "experiment": [checkpoint.parent.name],
+    }
+    for checkpoint in checkpoints
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="sf2_nethack",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
