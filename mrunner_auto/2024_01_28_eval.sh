e4b37887513985afb2098112f6ab3040b39cd20f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-02-01 12:18:21 +0100

    eval bcel

--- mrunner_auto/2024_01_28_eval.sh
+++ mrunner_auto/2024_01_28_eval.sh
@@ -6,24 +6,28 @@ ssh-add
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-KS-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-BCEL-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_APPO-EWC-T.py
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-KS-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-BCEL-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl4_APPO-EWC-T.py
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO-KS-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO-BCEL-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_dlvl8_APPO-EWC-T.py
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-KS-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-BC-T.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-BCEL-T.py
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_28_eval_sokoban_APPO-EWC-T.py
 
 # mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_01_31_eval_APPO.py
