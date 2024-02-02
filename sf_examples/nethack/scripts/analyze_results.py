bd3bf4c8fd98196ef73b85669d088a0b4ec4df76
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-02-02 19:39:56 +0100

    make a script from analyze results

--- sf_examples/nethack/scripts/analyze_results.py
+++ sf_examples/nethack/scripts/analyze_results.py
@@ -1,4 +1,5 @@
 import collections
+import sys
 from pathlib import Path
 
 import pandas as pd
@@ -21,22 +22,34 @@ def game_data_generator(xlogfile, filter=lambda x: x, separator="\t"):
             yield tuple(ctype(game_data[key]) for key, ctype in XLOGFILE_COLUMNS)
 
 
-eval_csv_path = Path("train_dir/monk-AA-BC_pretrained_use_prev_action/mon-hum-neu-mal_episodes20/eval_p0.csv")
-eval_df = pd.read_csv(eval_csv_path)
-eval_df["ttyrecname"] = eval_df["ttyrec"].apply(lambda x: Path(x).name)
+def get_xlogdata(ttyrec_path):
+    ttyrec_path = Path(ttyrec_path)
+    # right now assume that all ttyrecs are in the same folder
+    xlogdata = []
+    for xlogfile in reversed(sorted(filter(lambda p: p.suffix == ".xlogfile", ttyrec_path.iterdir()))):
+        sep = ":" if str(xlogfile).endswith(".txt") else "\t"
+        game_gen = game_data_generator(xlogfile, separator=sep)
+        for values in game_gen:
+            xlogdata.append(values)
 
-# right now assume that all ttyrecs are in the same folder
-xlogdata = []
-for xlogfile in reversed(
-    sorted(filter(lambda p: p.suffix == ".xlogfile", Path(eval_df["ttyrec"][0]).parent.iterdir()))
-):
-    sep = ":" if str(xlogfile).endswith(".txt") else "\t"
-    game_gen = game_data_generator(xlogfile, separator=sep)
-    for values in game_gen:
-        xlogdata.append(values)
+    xlogdf = pd.DataFrame(xlogdata, columns=list(map(lambda x: x[0], XLOGFILE_COLUMNS)))
+    xlogdf = xlogdf[["maxlvl", "role", "race", "gender", "align", "death", "ttyrecname"]]
 
-xlogdf = pd.DataFrame(xlogdata, columns=list(map(lambda x: x[0], XLOGFILE_COLUMNS)))
-xlogdf = xlogdf[["maxlvl", "role", "race", "gender", "align", "death", "ttyrecname"]]
+    return xlogdf
 
-merged_df = pd.merge(eval_df, xlogdf, on="ttyrecname", how="inner")
-merged_df.to_csv(eval_csv_path.parent / "merged_p0.csv")
+
+def main():
+    # eval_csv_path = Path("train_dir/monk-AA-BC_pretrained_use_prev_action/mon-hum-neu-mal_episodes20/eval_p0.csv")
+    eval_csv_path = Path("/home/bartek/mon-hum-neu-mal_episodes100/eval_p0.csv")
+    ttyrec_path = Path("/home/bartek/mon-hum-neu-mal_episodes100/nle_data")
+    eval_df = pd.read_csv(eval_csv_path)
+    eval_df["ttyrecname"] = eval_df["ttyrec"].apply(lambda x: Path(x).name)
+
+    xlogdf = get_xlogdata(ttyrec_path)
+
+    merged_df = pd.merge(eval_df, xlogdf, on="ttyrecname", how="inner")
+    merged_df.to_csv(eval_csv_path.parent / "merged_p0.csv")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
