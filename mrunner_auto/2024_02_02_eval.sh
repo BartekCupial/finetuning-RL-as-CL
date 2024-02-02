07962d860666fd98c9804ab34cd92bc28e666c3f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-02-02 12:10:16 +0100

    move configs to .icml

---
+++ mrunner_auto/2024_02_02_eval.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh-add
+
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_02_02_eval_full.py
+# mrunner --config ~/.mrunner.yaml --context athena_nethack_big_1gpu run mrunner_exps/eval/2024_02_02_eval_pretrain.py
