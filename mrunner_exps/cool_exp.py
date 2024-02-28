22a7d858b2b658fc309a5c5d2b02ccc2e032599e
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-02-28 17:34:33 +0100

    cool exp

---
+++ mrunner_exps/cool_exp.py
@@ -0,0 +1,40 @@
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "env": "challenge",
+    "exp_tags": [name],
+    "exp_point": "monk-APPO",
+    "train_for_env_steps": 1_000_000,
+    "group": "monk-APPO",
+    "character": "mon-hum-neu-mal",
+    "num_workers": 16,
+    "num_envs_per_worker": 16,
+    "worker_num_splits": 2,
+    "rollout": 32,
+    "batch_size": 1024,  # this equals bs = 128, 128 * 32 = 4096
+    "async_rl": True,
+    "serial_mode": False,
+    "use_prev_action": True,
+}
+
+# params different between exps
+params_grid = [
+    {
+        "seed": list(range(1)),
+    },
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="sf2_nethack",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
