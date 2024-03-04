84ea1c135f5901543c235f0618e8ed0bef2690ed
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-03-04 14:00:18 +0100

    update fine tuning config

--- mrunner_exps/monk-APPO-T.py
+++ mrunner_exps/monk-APPO-T.py
@@ -7,38 +7,52 @@ config = {
     "env": "challenge",
     "exp_tags": [name],
     "exp_point": "monk-APPO-T",
-    "train_for_env_steps": 1_000_000_000,
+    "train_for_env_steps": 500_000_000,
     "group": "monk-APPO-T",
     "character": "mon-hum-neu-mal",
-    "num_workers": 8,
+    "num_workers": 16,
     "num_envs_per_worker": 16,
     "worker_num_splits": 2,
     "rollout": 32,
-    "batch_size": 512,  # this equals bs = 128, 128 * 32 = 4096
+    "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
     "async_rl": True,
     "serial_mode": False,
     "wandb_user": "bartekcupial",
     "wandb_project": "sf2_nethack",
     "wandb_group": "gmum",
     "with_wandb": True,
-    "use_pretrained_checkpoint": True,
-    "model_path": "train_dir/amzn-AA-BC_pretrained",
     "use_prev_action": True,
     "model": "ScaledNet",
     "use_resnet": True,
     "rnn_size": 1738,
     "h_dim": 1738,
-    "learning_rate": 0.0001,
-    "freeze": {"encoder": 0},
+    "gamma": 1.0,
+    "skip_train": 25_000_000,
+    "use_pretrained_checkpoint": True,
+    "model_path": "/net/pr2/projects/plgrid/plgggmum_crl/bcupial/sf_checkpoints/pretrain_critic_01_18",
+    "lr_schedule": "linear_decay",
+    "heartbeat_interval": 600,
+    "heartbeat_reporting_interval": 1200,
 }
 
-# params different between exps
+expected_batch_size = 4096
+rollout = 256
+target_batch_size = 256
+
+batch_size = min(expected_batch_size, min(target_batch_size * rollout, expected_batch_size * 8))
+batches_to_accumulate = max(1, (rollout * target_batch_size) // expected_batch_size)
+optim_step_every_ith = max(1, batches_to_accumulate // 8)
 params_grid = [
     {
         "seed": list(range(1)),
-        "restart_behavior": ["overwrite"],
-        "with_wandb": [False],
-    },
+        "learning_rate": [0.0001],
+        "freeze": [{"encoder": 0}],
+        "rollout": [rollout],
+        "batch_size": [batch_size],  # 32 * 512, 64 * 256, 128 * 128
+        "num_batches_per_epoch": [min(8, batches_to_accumulate)],
+        "optim_step_every_ith": [optim_step_every_ith],
+        "target_batch_size": [target_batch_size],
+    }
 ]
 
 experiments_list = create_experiments_helper(
